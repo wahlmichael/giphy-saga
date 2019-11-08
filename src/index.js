09c01a586d02b14a9de10de31cb0c24237350b63
Michael Wahl <michaelwahl9@gmail.com>
2019-11-08 11:53:23 -0600

    add get gif saga

--- src/index.js
+++ src/index.js
@@ -5,14 +5,34 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects'; 
+import axios from 'axios';
 
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
+
+function* searchGiphy(action){
+    try{
+      yield axios.get('/search',{
+          params: {
+              searchTerm: action.payload,
+          }
+      });
+    } catch {
+     console.log('Error in searchGiphy');
+    }
+  }
+
+  function* rootSaga() {
+    yield takeEvery('SEARCH_PLANT', searchGiphy);
+   }
 
 const store = createStore(
     combineReducers({ 
      }),
      applyMiddleware(sagaMiddleware, logger)
   );
+
+  sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
