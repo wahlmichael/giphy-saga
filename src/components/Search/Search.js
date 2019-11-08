1577ccfe8d9e67789e600aa691e2181f15d00d28
Iris Noh <irisnoh22@gmail.com>
2019-11-08 11:50:19 -0600

    first commit, created input and button

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import {withRouter} from 'react-router-dom';
+import { connect } from 'react-redux'
 
 class Search extends Component {
+  state= {
+    search: '',
+  }
+
+  searchFunction = (event) => {
+    this.setState({
+      search: event.target.value
+    })
+  }
+
+  submitSearch = () => {
+    // this.props.dispatch({type: "SEARCH_GIPHY", payload: this.state.search})
+    console.log('hit submit')
+  }
+
   render() {
+  
     return (
       <div>
         <h1>Search for some Gifs</h1>
+
+        <input placeholder="search" value={this.state.search} onChange ={this.searchFunction}/>
+        <button onClick={this.submitSearch}>Submit</button>
       </div>
     );
   }
 }
 
-export default withRouter(Search);
+export default connect()(withRouter(Search));
